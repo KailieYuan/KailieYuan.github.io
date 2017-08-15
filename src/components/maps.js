1321a6fafaa87bde59c491893fff57748a078936
Ryan Ferguson <fergusman@gmail.com>
2017-08-15 13:44:35 -0400

    added map component

--- src/components/maps.js
+++ src/components/maps.js
@@ -1,14 +1,27 @@
 import React from 'react'
-
-class Maps extends React.Component {
-
-
+import { Map, Marker, TileLayer, Popup } from 'react-leaflet';
+import "../App.css"
+const mapCenter = [40.761722, -73.949628];
+const zoomLevel = 15; 
+const attribution = '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
+const mapUrl = 'http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png'
+class Mapper extends React.Component {
 
   render() {
     return (
-      <div></div>
+      <div> 
+        
+        <Map center={mapCenter} zoom={zoomLevel} className="leaflet-container">
+          <TileLayer url= {mapUrl} attribution={attribution} />
+          <Marker position={mapCenter}>
+            <Popup>
+            <span><br/>Senior Center<br/><br/></span>
+          </Popup>
+          </Marker>
+      </Map>
+      </div>
     )
   }
 }
 
-export default Maps 
+export default Mapper 
