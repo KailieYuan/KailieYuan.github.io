37e1bdb45f9e78ff7cafe4d6c5a99cf6b55af6fd
Connie Liu <cliu@cbinsights.com>
2017-08-15 14:31:26 -0400

    update home page with images

--- src/container/Home.js
+++ src/container/Home.js
@@ -1,46 +1,69 @@
 import React, { Component } from 'react';
 import '../App.css';
-import {Card, Col} from 'react-materialize'
+import {Card, CardTitle, Col} from 'react-materialize'
 import { Link } from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
       <div className="container">
-    
-    <div className="row">
-     <Col m={6} s={12}>
-		  <Card className='blue-grey darken-1' textClassName='white-text' title='Maps' actions={[<Link to="/">THIS IS A LINK</Link>]}>
-		    I am a very simple clickable card to go to maps 
-		  </Card>
-    </Col> 
-    <Col m={6} s={12}>
-		  <Card className='blue-grey darken-1' textClassName='white-text' title='Chat' actions={[<a href='#'>THIS IS A LINK</a>]}>
-		    I am a very simple clickable card to go to chat
-		  </Card>
-    </Col> 
-    <Col m={6} s={12}>
-		  <Card className='blue-grey darken-1' textClassName='white-text' title='Exercise' actions={[<a href='#'>THIS IS A LINK</a>]}>
-		    I am a very simple clickable card to go to exercise  
-		  </Card>
-    </Col> 
-    <Col m={6} s={12}>
-		  <Card className='blue-grey darken-1' textClassName='white-text' title='Games' actions={[<a href='#'>THIS IS A LINK</a>]}>
-		    I am a very simple clickable card to go to games 
-		  </Card>
-    </Col> 
-    <Col m={6} s={12}>
-		  <Card className='blue-grey darken-1' textClassName='white-text' title='Calendar' actions={[<a href='#'>THIS IS A LINK</a>]}>
-		    I am a very simple clickable card to go to calendar 
-		  </Card>
-    </Col> 
-    <Col m={6} s={12}>
-		  <Card className='blue-grey darken-1' textClassName='white-text' title='Music' actions={[<a href='#'>THIS IS A LINK</a>]}>
-		    I am a very simple clickable card to go to music 
-		  </Card>
-    </Col> 
+        <div className="row">
+            <Col m={6} s={12}>
+                <Card
+                    className='small blue lighten-5'
+                    textClassName='black-text' title="Events Calender" header={<CardTitle
+                    image={require('../images/cards_cal.jpg')} waves='light' />}
+                    actions={[<Link to="/">Events Calendar</Link>]}
+                    >
+                </Card>
+            </Col> 
+             <Col m={6} s={12}>
+                <Card
+                    className='small blue lighten-5'
+                    textClassName='black-text' title="Maps" header={<CardTitle
+                    image={require('../images/cards_map.jpg')} waves='light' />}
+                    actions={[<Link to="/">Maps</Link>]}
+                    >
+                </Card>
+            </Col> 
+            <Col m={6} s={12}>
+                <Card
+                    className='small blue lighten-5'
+                    textClassName='black-text' title="Chat" header={<CardTitle
+                    image={require('../images/cards_chat.jpg')} waves='light' />}
+                    actions={[<Link to="/">Chat</Link>]}
+                    >
+                </Card>
+            </Col>
+            <Col m={6} s={12}>
+               <Card
+                    className='small blue lighten-5'
+                    textClassName='black-text' title="Exercise" header={<CardTitle
+                    image={require('../images/cards_exercise.jpg')} waves='light' />}
+                    actions={[<Link to="/">Exercise</Link>]}
+                    >
+                </Card>
+            </Col> 
+            <Col m={6} s={12}>
+        	   <Card
+                    className='small blue lighten-5'
+                    textClassName='black-text' title="Games" header={<CardTitle
+                    image={require('../images/cards_games.jpg')} waves='light' />}
+                    actions={[<Link to="/">Games</Link>]}
+                    >
+                </Card>
+            </Col> 
+            <Col m={6} s={12}>
+                <Card
+                    className='small blue lighten-5'
+                    textClassName='black-text' title="Music" header={<CardTitle
+                    image={require('../images/cards_music.jpg')} waves='light' />}
+                    actions={[<Link to="/">Music</Link>]}
+                    >
+                </Card>
+            </Col> 
+        </div>
     </div>
-    </div>   
     );
   }
 }
