31d7cf097f5cf74ff4f8ff62d4070d7848a48967
Ryan Ferguson <fergusman@gmail.com>
2017-08-15 11:32:38 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import './App.css';
+import Map from './components/maps.js'
+import {Card, Col} from 'react-materialize'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+    <div className="row">
+     <Col m={6} s={12}>
+		  <Card className='blue-grey darken-1' textClassName='white-text' title='Maps' actions={[<a href='#'>THIS IS A LINK</a>]}>
+		    I am a very simple clickable card to go to maps 
+		  </Card>
+    </Col> 
+    <Col m={6} s={12}>
+		  <Card className='blue-grey darken-1' textClassName='white-text' title='Chat' actions={[<a href='#'>THIS IS A LINK</a>]}>
+		    I am a very simple clickable card to go to chat
+		  </Card>
+    </Col> 
+    <Col m={6} s={12}>
+		  <Card className='blue-grey darken-1' textClassName='white-text' title='Exercise' actions={[<a href='#'>THIS IS A LINK</a>]}>
+		    I am a very simple clickable card to go to exercise  
+		  </Card>
+    </Col> 
+    <Col m={6} s={12}>
+		  <Card className='blue-grey darken-1' textClassName='white-text' title='Games' actions={[<a href='#'>THIS IS A LINK</a>]}>
+		    I am a very simple clickable card to go to games 
+		  </Card>
+    </Col> 
+    <Col m={6} s={12}>
+		  <Card className='blue-grey darken-1' textClassName='white-text' title='Calendar' actions={[<a href='#'>THIS IS A LINK</a>]}>
+		    I am a very simple clickable card to go to calendar 
+		  </Card>
+    </Col> 
+    <Col m={6} s={12}>
+		  <Card className='blue-grey darken-1' textClassName='white-text' title='Music' actions={[<a href='#'>THIS IS A LINK</a>]}>
+		    I am a very simple clickable card to go to music 
+		  </Card>
+    </Col> 
+    </div>
+    </div>   
+    );
+  }
+}
+
+export default App;
---
+++ src/components/maps.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class Maps extends React.Component {
+
+
+
+  render() {
+    return (
+      <div></div>
+    )
+  }
+}
+
+export default Maps 
