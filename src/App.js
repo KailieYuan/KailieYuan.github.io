b1242357983b6aa627ef5eb5dbc6a66c8fe9265d
Ryan Ferguson <fergusman@gmail.com>
2017-08-15 11:48:28 -0400

    this added home container and basic react routing

--- src/App.js
+++ src/App.js
@@ -1,43 +1,28 @@
 import React, { Component } from 'react';
 import './App.css';
 import Map from './components/maps.js'
+import Home from './container/Home.js'
 import {Card, Col} from 'react-materialize'
+import {
+  Route,
+  Link
+} from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
       <div className="container">
-    <div className="row">
-     <Col m={6} s={12}>
-		  <Card className='blue-grey darken-1' textClassName='white-text' title='Maps' actions={[<a href='#'>THIS IS A LINK</a>]}>
+         <div className="row">
+        <Col m={6} s={12}>
+		  <Card className='blue-grey darken-1' textClassName='white-text' title='English' actions={[<a href='#'>THIS IS A LINK</a>]}>
 		    I am a very simple clickable card to go to maps 
 		  </Card>
     </Col> 
     <Col m={6} s={12}>
-		  <Card className='blue-grey darken-1' textClassName='white-text' title='Chat' actions={[<a href='#'>THIS IS A LINK</a>]}>
+		  <Card className='blue-grey darken-1' textClassName='white-text' title='Korean' actions={[<a href='#'>THIS IS A LINK</a>]}>
 		    I am a very simple clickable card to go to chat
 		  </Card>
-    </Col> 
-    <Col m={6} s={12}>
-		  <Card className='blue-grey darken-1' textClassName='white-text' title='Exercise' actions={[<a href='#'>THIS IS A LINK</a>]}>
-		    I am a very simple clickable card to go to exercise  
-		  </Card>
-    </Col> 
-    <Col m={6} s={12}>
-		  <Card className='blue-grey darken-1' textClassName='white-text' title='Games' actions={[<a href='#'>THIS IS A LINK</a>]}>
-		    I am a very simple clickable card to go to games 
-		  </Card>
-    </Col> 
-    <Col m={6} s={12}>
-		  <Card className='blue-grey darken-1' textClassName='white-text' title='Calendar' actions={[<a href='#'>THIS IS A LINK</a>]}>
-		    I am a very simple clickable card to go to calendar 
-		  </Card>
-    </Col> 
-    <Col m={6} s={12}>
-		  <Card className='blue-grey darken-1' textClassName='white-text' title='Music' actions={[<a href='#'>THIS IS A LINK</a>]}>
-		    I am a very simple clickable card to go to music 
-		  </Card>
-    </Col> 
+    </Col>
     </div>
     </div>   
     );
--- src/index.js
+++ src/index.js
@@ -2,7 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Home from './container/Home.js'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+<Router>
+  <div>
+  <Route exact path="/" component={App}/>
+  <Route exact path="/home" component={Home}/>
+  <Route path="/map" component={Map}/>
+  </div>
+</Router>, document.getElementById('root'));
 registerServiceWorker();
