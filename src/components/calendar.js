42360780b2fe2dada5a176ab5d4c126bc8155543
Connie Liu <cliu@cbinsights.com>
2017-08-16 10:30:05 -0400

    update calendar

--- src/components/calendar.js
+++ src/components/calendar.js
@@ -5,9 +5,10 @@ class Calendar extends React.Component {
   render() {
     return (
       <div className="container"> 
-        <iframe
-          src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=a8b02rsucen44ko43uoqs2ukc8%40group.calendar.google.com&amp;color=%232952A3&amp;ctz=America%2FNew_York"
-          width="800" height="600" frameborder="0" scrolling="no"></iframe>
+      <div className="row"> 
+      <iframe src="https://calendar.google.com/calendar/embed?mode=WEEK&amp;height=800&amp;wkst=1&amp;bgcolor=%23ccccff&amp;src=a8b02rsucen44ko43uoqs2ukc8%40group.calendar.google.com&amp;color=%232952A3&amp;ctz=America%2FNew_York"
+      	width="1000" height="800" frameborder="0" scrolling="no"></iframe>
+      	</div> 
       </div>
     )
   }
