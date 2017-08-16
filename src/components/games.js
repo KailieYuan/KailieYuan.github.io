e433e77f34f36f0ada2773b03556201d86531aaa
Ryan Ferguson <fergusman@gmail.com>
2017-08-16 09:35:21 -0400

    added games images

--- src/components/games.js
+++ src/components/games.js
@@ -18,6 +18,33 @@ class Games extends React.Component {
                     actions={[<Link to="/games/colorflood">Click here to play</Link>]}
                     >
                 </Card>
+            </Col>
+            <Col m={6} s={12}>
+                <Card
+                    className='small blue lighten-5'
+                    textClassName='black-text' title="Trivia!" header={<CardTitle
+                    image={require('../images/Jeopardy.jpg')} waves='light' />}
+                    actions={[<Link to="/games">Click here to play</Link>]}
+                    >
+                </Card>
+            </Col> 
+            <Col m={6} s={12}>
+                <Card
+                    className='small blue lighten-5'
+                    textClassName='black-text' title="Sudoku!" header={<CardTitle
+                    image={require('../images/sudoku.jpg')} waves='light' />}
+                    actions={[<Link to="/games">Click here to play</Link>]}
+                    >
+                </Card>
+            </Col> 
+            <Col m={6} s={12}>
+                <Card
+                    className='small blue lighten-5'
+                    textClassName='black-text' title="Add More Games!" header={<CardTitle
+                    image={require('../images/add_more.png')} waves='light' />}
+                    actions={[<Link to="/games">Click here to play</Link>]}
+                    >
+                </Card>
             </Col> 
           </div>
         </div>
